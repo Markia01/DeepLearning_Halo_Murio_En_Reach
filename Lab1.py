e3ce19cd1e10b37d483bcca0c6b5cbc39b038487
Kaleb <hekamartinezhe@gmail.com>
2025-09-15 23:13:29 -0700

    add rating variables for every user

---
+++ Lab1.py
@@ -0,0 +1,14 @@
+#!Similitud Coseno para 2 usuarios iterada a 3
+
+
+# Calificaciones de Pepito
+PSherk = 5
+PJackass = 5
+
+# Calificaciones Ramona
+RSherk = 1
+RJackass = None
+
+# Calificaciones Yakitori
+YSherk = 4
+YJackass = 2
