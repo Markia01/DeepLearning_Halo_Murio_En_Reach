728b8d65cfaabb7bb82a2e551e045ebae12e057d
Markia0 <tic-320124@utnay.edu.mx>
2025-09-16 00:22:41 -0700

    add similarity calculation for yakitori  and Ramona

--- Lab1.py
+++ Lab1.py
@@ -1,19 +1,25 @@
-#!Similitud Coseno para 2 usuarios iterada a 3
+#!Similitud Coseno para 2 usuarios iterada a 3 y predicción
 
 
-# Calificaciones de Pepito
+#Calificaciones de Pepito
 PSherk = 5
 PJackass = 5
 
-# Calificaciones Ramona
+#Calificaciones Ramona
 RSherk = 1
 RJackass = None
 
-# Calificaciones Yakitori
+#Calificaciones Yakitori
 YSherk = 4
 YJackass = 2
 
-# Similitud entre pepito y ramona
+
+#Similitud entre pepito y ramona
 numerador =  PSherk * RSherk
-denominador = (PSherk**2)**0.5 * (RSherk**2)**0.5
+denominador = (PSherk ** 2) ** 0.5 (RSherk ** 2)** 0.5
 similitud = numerador/denominador
+
+#Similitud entre Yakitori y ramona
+numerador = YSherk * RSherk 
+denominador = (YSherk ** 2) ** 0.5 (RSherk ** 2) ** 0.5
+similitud = numerador/denominador 
