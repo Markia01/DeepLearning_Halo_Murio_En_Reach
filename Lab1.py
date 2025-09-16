6ce36c2a2089f80ff014f34109833626f341ec5a
HilkirySG <ulijesusgongora@gmail.com>
2025-09-16 01:02:06 -0700

    add prediction quotient calculations

--- Lab1.py
+++ Lab1.py
@@ -1,35 +1,32 @@
-#!Similitud Coseno para 2 usuarios iterada a 3 y predicción
+#!Similitud Coseno para 2 usuarios iterada a 3
 
-
-#Calificaciones de Pepito
+# Calificaciones de Pepito
 PSherk = 5
 PJackass = 5
 
-#Calificaciones Ramona
+# Calificaciones Ramona
 RSherk = 1
 RJackass = None
 
-#Calificaciones Yakitori
+# Calificaciones Yakitori
 YSherk = 4
 YJackass = 2
 
-
-#Similitud entre pepito y ramona
+# Similitud entre pepito y ramona
 numerador =  PSherk * RSherk
-denominador = (PSherk ** 2) ** 0.5 (RSherk ** 2)** 0.5
+denominador = (PSherk**2)**0.5  * (RSherk**2)**0.5
 similitud = numerador/denominador
 
-#Similitud entre Yakitori y ramona
-numerador = YSherk * RSherk 
-denominador = (YSherk ** 2) ** 0.5 (RSherk ** 2) ** 0.5
-similitud_2 = numerador/denominador 
 
-#Prediccion de Ramona(Jackass):
+#Similitud entre pepito y yakitori
+numerador2 = PSherk * YSherk 
+denominador2 = (PSherk**2)**0.5 * (YSherk**2)**0.5
+
+similitud2 = numerador2/denominador2
 
 
-# El numerador es la suma de las calificaciones de los otros para 'Jackass', ponderadas por su similitud
-numerador_prediccion = (similitud * PJackass) + (similitud_2 * YJackass)
-# El denominador es la suma de las similitudes
-denominador_prediccion = similitud + similitud_2 
-# La predicción es la división de ambos.
-prediccion_RJackass = numerador_prediccion / denominador_prediccion
+#Prediccion Ramona
+prediccionR = (similitud * PJackass + similitud2 * YJackass) / (similitud + similitud2)
+
+#Resultados de la prediccion
+print("La prediccion de Ramona es: ", round(prediccionR,2))
