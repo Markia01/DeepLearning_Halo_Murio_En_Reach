6c85fefb4c6c515b7b2065069cab1822e6964c1f
Julissa <velazquezdalu@gmail.com>
2025-09-16 00:48:47 -0700

    add prediction quotient calculations

--- Lab1.py
+++ Lab1.py
@@ -22,4 +22,14 @@ similitud = numerador/denominador
 #Similitud entre Yakitori y ramona
 numerador = YSherk * RSherk 
 denominador = (YSherk ** 2) ** 0.5 (RSherk ** 2) ** 0.5
-similitud = numerador/denominador 
+similitud_2 = numerador/denominador 
+
+#Prediccion de Ramona(Jackass):
+
+
+# El numerador es la suma de las calificaciones de los otros para 'Jackass', ponderadas por su similitud
+numerador_prediccion = (similitud * PJackass) + (similitud_2 * YJackass)
+# El denominador es la suma de las similitudes
+denominador_prediccion = similitud + similitud_2 
+# La predicción es la división de ambos.
+prediccion_RJackass = numerador_prediccion / denominador_prediccion
