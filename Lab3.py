cb3904b062482bb9682f8f9bdc5c42adc31d5712
Julissa <velazquezdalu@gmail.com>
2025-09-30 23:17:35 -0700

    fix typo

--- Lab3.py
+++ Lab3.py
@@ -7,14 +7,4 @@ input_data = [
     [1, 1]
 ]
 
-
-# perceptron used for binary classification for NAND logic gates
-
-input_data = [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
-]
-
 w1, w2, b = -1,-1 , 1.5
