18ec58b88486e870ae1af210b887009782ef8c31
Julissa <velazquezdalu@gmail.com>
2025-09-30 23:14:07 -0700

    fix kaleb's mess

--- Lab3.py
+++ Lab3.py
@@ -8,13 +8,13 @@ input_data = [
 ]
 
 
-def not_heaviside(x): return 1 if x <= 0 else 0
+# perceptron used for binary classification for NAND logic gates
 
+input_data = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+]
 
-expected_output = [1, 1, 1, 0]  # AND logic gate
-weights = [1, 1]
-bias = -1.5
-for x in input_data:
-    linear_combination = sum(w*i for w, i in zip(weights, x)) + bias
-    prediction = not_heaviside(linear_combination)
-    print(f"Input: {x}, Prediction: {prediction}")
+w1, w2, b = -1,-1 , 1.5
