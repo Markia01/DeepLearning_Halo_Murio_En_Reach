ab2ef638c40a97f5a11580fef497a53846e0562f
HilkirySG <ulijesusgongora@gmail.com>
2025-10-08 21:51:20 -0700

    Lineal output

--- Lab4_Sigmoid.py
+++ Lab4_Sigmoid.py
@@ -2,10 +2,14 @@ import numpy as np
 
 rand = np.random.seed(42)
 
+
 W = np.random.rand(4, 4)
 b = np.random.rand(4, 1)
-X = np.random.rand(4, 1)
+x = np.random.rand(4, 1)
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+#* Feedforward (producto punto entrada * pesos)
+y_lineal = x @ W  # Esta es la salida lineal antes de la activación
