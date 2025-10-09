53fd1bac4e130cd0685d15249f2a18e0a8d74ed8
tecnomus <tecnomus2@gmail.com>
2025-10-08 22:07:17 -0700

    Definicion de la funcion de activacion sigmoide

--- Lab4_Sigmoid.py
+++ Lab4_Sigmoid.py
@@ -9,3 +9,6 @@ rng = np.random.default_rng(42)
 W = rng.random((3, 4))  # Matriz de pesos 3x4
 x = np.array([1.0, 0.5, -1.0]) # Vector de entrada (3 valores)
 
+#* Definimos la función de activación sigmoide
+def sigmoid(z):
+  return 1 / (1 + np.exp(-z))
