7be338ea05bcb3fc5a42bae2cbc181cb624263f7
HilkirySG <ulijesusgongora@gmail.com>
2025-10-08 21:41:50 -0700

    First touch to the code lines to create a Artificial Neuron with ReLu (Fixed)

--- Lab4_ReLu.py
+++ Lab4_ReLu.py
@@ -10,3 +10,16 @@ rng = np.random.default_rng(42)
 #* 3 entradas → 4 salidas
 W = rng.random((3, 4))   # Matriz de pesos 3x4
 x = np.array([1.0, 0.5, -1.0])  # Vector de entrada (3 valores)
+
+
+#* Feedforward (producto punto entrada * pesos)
+y = x @ W  # Equivalente a np.dot(x, W)
+#*np.dot()
+
+#* Mostrar resultados
+print("Matriz de pesos (3x4):")
+print(W)
+print("\nEntrada:")
+print(x)
+print("\nSalida:")
+print(y) #Ñaca
