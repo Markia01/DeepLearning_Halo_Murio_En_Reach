e619a6f6749a73ee63ba116770282a553f1b098c
HilkirySG <ulijesusgongora@gmail.com>
2025-10-28 23:11:53 -0700

    Translate comments to English in Lab6.py and print the result with the output layer
    Updated comments in Lab6.py from Spanish to English for better clarity.

--- Lab6.py
+++ Lab6.py
@@ -25,22 +25,28 @@ b2= tf.Variable(tf.random.normal([3]), name='bias2')
 w_out= tf.Variable(tf.random.normal([3,3]), name='weight out')
 b_out= tf.Variable(tf.random.normal([3]), name='bias out')
 
-# --- Cálculos de la Red Neuronal ---
+# ---   Neural Network Calculations ---
 
 print("Tensor de Entrada (X):")
 print(X.numpy())
 
-## 1. Cálculo de la Capa Oculta 1 (con ReLU)
-# Usamos tf.matmul para la multiplicación de matrices (Entrada * Pesos) + Sesgo
+## 1. Hidden Layer 1 (with ReLU)
+# using tf.matmul function to multiplicate  (entry * weight) + bias
 z1 = tf.matmul(X, w1) + b1
-# Aplicamos la activación ReLU
+# RELU Activation
 h1 = relu(z1)
 
 print("\n--- Activación Capa Oculta 1 (ReLU) ---")
 print(h1.numpy())
 
-## 2. Cálculo de la Capa Oculta 2 (con Sigmoide)
-# La entrada a esta capa es la salida de la capa anterior (h1)
+## 2. Hidden Layer 2 (with Sigmoid)
+# The previous layer is the entry to the second one (h1)
 z2 = tf.matmul(h1, w2) + b2
-# Aplicamos la activación Sigmoide
+# Sigmoid Activation
 h2 = sigmoid(z2)
+
+y_activada=h2.numpy()
+
+print("Salida de la red neuronal (3 muestras , 2 salidas cada una)----:")
+print(y_activada)
+print("\n")
