343e36dfc35568f928f2b850ee69a8bcb17bbd5c
Markia0 <tic-320124@utnay.edu.mx>
2025-10-28 22:47:31 -0700

    added the hidden layers and the exit layer

--- Lab6.py
+++ Lab6.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
 # Define a 3x3 input tensor
-X = tf.constant([[1.0, 2.0, 3.0], [3.0, 4.0, 5.0], [5.0, 6.0, 7.0]])
+X = tf.constant([[1.0, 2.0, 3.0],
+                 [3.0, 4.0, 5.0], 
+                 [5.0, 6.0, 7.0]])
 
 # Define reLu activation
 def relu(x):
@@ -11,5 +13,14 @@ def relu(x):
 def sigmoid(x):
     return tf.nn.sigmoid(x)
 
-w1 = tf.Variable(tf.random.normal([3, 2]), name='weight1')
-b1 = tf.Variable(tf.random.normal([2]), name='bias1')
+#define hidden layer #1
+w1 = tf.Variable(tf.random.normal([3, 3]), name='weight1')
+b1 = tf.Variable(tf.random.normal([3]), name='bias1')
+
+#define hidden layer #2
+w2= tf.Variable(tf.random.normal([3,3]), name='weight2')
+b2= tf.Variable(tf.random.normal([3]), name='bias2')
+
+#define out layer
+w_out= tf.Variable(tf.random.normal([3,3]), name='weight out')
+b_out= tf.Variable(tf.random.normal([3]), name='bias out')
