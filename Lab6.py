be1e60e76dc4e67d50083fc569f814e9481ea24d
HilkirySG <ulijesusgongora@gmail.com>
2025-10-28 23:21:44 -0700

    Refactor neural network layer definitions and outputs

--- Lab6.py
+++ Lab6.py
@@ -13,40 +13,54 @@ def relu(x):
 def sigmoid(x):
     return tf.nn.sigmoid(x)
 
-#define hidden layer #1
+# --- Layer Definitions ---
+
+# define hidden layer #1 (Input: 3, Output: 3)
 w1 = tf.Variable(tf.random.normal([3, 3]), name='weight1')
 b1 = tf.Variable(tf.random.normal([3]), name='bias1')
 
-#define hidden layer #2
-w2= tf.Variable(tf.random.normal([3,3]), name='weight2')
-b2= tf.Variable(tf.random.normal([3]), name='bias2')
+# define hidden layer #2 (Input: 3, Output: 3)
+w2 = tf.Variable(tf.random.normal([3, 3]), name='weight2')
+b2 = tf.Variable(tf.random.normal([3]), name='bias2')
+    
+# define out layer (Input: 3, Output: 2)
+w_out = tf.Variable(tf.random.normal([3, 2]), name='weight_out')
+b_out = tf.Variable(tf.random.normal([2]), name='bias_out')
 
-#define out layer
-w_out= tf.Variable(tf.random.normal([3,3]), name='weight out')
-b_out= tf.Variable(tf.random.normal([3]), name='bias out')
 
-# ---   Neural Network Calculations ---
+# --- Neural Network Calculations ---
 
-print("Tensor de Entrada (X):")
+print("Input Tensor (X):")
 print(X.numpy())
+print("Shape:", X.shape)
 
 ## 1. Hidden Layer 1 (with ReLU)
-# using tf.matmul function to multiplicate  (entry * weight) + bias
+# (Input: 3, Output: 3)
 z1 = tf.matmul(X, w1) + b1
-# RELU Activation
 h1 = relu(z1)
 
-print("\n--- Activación Capa Oculta 1 (ReLU) ---")
+print("\n--- Hidden Layer 1 Activation (ReLU) ---")
 print(h1.numpy())
+print("Shape:", h1.shape)
+
 
 ## 2. Hidden Layer 2 (with Sigmoid)
-# The previous layer is the entry to the second one (h1)
+# (Input: 3, Output: 3)
 z2 = tf.matmul(h1, w2) + b2
-# Sigmoid Activation
 h2 = sigmoid(z2)
 
-y_activada=h2.numpy()
+print("\n--- Hidden Layer 2 Activation (Sigmoid) ---")
+print(h2.numpy())
+print("Shape:", h2.shape)
 
-print("Salida de la red neuronal (3 muestras , 2 salidas cada una)----:")
-print(y_activada)
-print("\n")
+
+## 3. Output Layer (No activation in this example)
+# (Input: 3, Output: 2)
+z_out = tf.matmul(h2, w_out) + b_out
+# 'y_final' is now the final output
+y_final = z_out.numpy() 
+
+
+print("\n--- Neural Network Output (3 samples, 2 outputs each) ---")
+print(y_final)
+print("Output shape:", y_final.shape)
