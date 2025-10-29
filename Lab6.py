2d82876e6e2791ceb875a73433c26565b7cb517a
Julissa <velazquezdalu@gmail.com>
2025-10-28 22:31:04 -0700

    add first layer elements and comments

--- Lab6.py
+++ Lab6.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 
+# Define a 3x3 input tensor
 X = tf.constant([[1.0, 2.0, 3.0], [3.0, 4.0, 5.0], [5.0, 6.0, 7.0]])
 
-
+# Define reLu activation
 def relu(x):
     return tf.nn.relu(x)
 
-
+# Define sigmoid activation function
 def sigmoid(x):
     return tf.nn.sigmoid(x)
+
+w1 = tf.Variable(tf.random.normal([3, 2]), name='weight1')
+b1 = tf.Variable(tf.random.normal([2]), name='bias1')
