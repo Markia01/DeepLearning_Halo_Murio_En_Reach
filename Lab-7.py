d6c5790bdd8e4ed1a72f74892cc2720240ee0653
Julissa <velazquezdalu@gmail.com>
2025-11-04 21:37:40 -0700

    add inputsand hiddenlayers

--- Lab-7.py
+++ Lab-7.py
@@ -17,3 +17,19 @@ def tanh(x):
     """Aplica la activación Tangente Hiperbólica (Tanh)."""
     # Esta es la tercera función que pedía la imagen.
     return tf.nn.tanh(x)
+
+# - 2. Definición de la Red Neuronal (Arquitectura 2-3-3) 
+
+# Entradas (Inputs)
+X = tf.constant([
+    [1.0, 2.0],
+    [0.5, -1.0],
+    [-2.0, 1.5],
+    [0.0, -0.5]
+], dtype=tf.float32)
+
+# Capa Oculta (1 capa con 3 neuronas)
+w1 = tf.Variable(tf.random.normal([2, 3]), name='weight1')
+b1 = tf.Variable(tf.random.normal([3]), name='bias1')
+
+
