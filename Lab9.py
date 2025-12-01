8a9cf6a69d773562877ae11ec4c69df9e7fbe5d0
HilkirySG <ulijesusgongora@gmail.com>
2025-11-30 18:08:03 -0700

    Create proyect and def solution

---
+++ Lab9.py
@@ -0,0 +1,9 @@
+import random
+import random
+#Crear una solucion al azar (0-100)
+def crear_individuo():
+    return [random.randint(0, 10)]
+
+#Calcular el fitness de un individuo
+def  fitness(x):
+    return x
